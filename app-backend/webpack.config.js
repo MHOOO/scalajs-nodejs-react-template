6d4470007254fced2904817df541cf0b0157b2ad
Thomas Karolski <Thomas.Karolski@googlemail.com>
2016-12-13 08:18:29 +0100

    Fix webpack config for backend
    Help from:
    http://stackoverflow.com/questions/31102035/how-can-i-use-webpack-with-express

--- app-backend/webpack.config.js
+++ app-backend/webpack.config.js
@@ -1,6 +1,18 @@
 var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
 
 module.exports = require('./scalajs.webpack.config');
 
 // make sure all deps are for nodejs
 module.exports.target = "node"
+
+// note the path.resolve(__dirname, ...) part
+// without it, eslint-import-resolver-webpack fails
+// since eslint might be invoked with different cwd
+var nodeModules = {};
+fs.readdirSync(path.resolve(__dirname, 'node_modules'))
+    .filter(x => ['.bin'].indexOf(x) === -1)
+    .forEach(mod => { nodeModules[mod] = `commonjs ${mod}`; });
+
+module.exports.externals = nodeModules
