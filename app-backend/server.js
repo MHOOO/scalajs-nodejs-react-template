b4d9f7681070490e08292ef7cb3c9c89c42c48ac
Thomas Karolski <Thomas.Karolski@googlemail.com>
2016-12-13 09:44:59 +0100

    mod nodejs bootstrap code

--- app-backend/server.js
+++ app-backend/server.js
@@ -4,8 +4,11 @@
 (function () {
     // require("./target/scala-2.11/scalajsbundler-deps.js");
     // require("./target/scala-2.11/scalajsbundler-entry-point.js");
+    var app = require("./target/scala-2.11/app-backend-fastopt-bundle.js");
     // var app = require("./target/scala-2.11/app-backend-fastopt.js").__ScalaJSExportsNamespace.app;
-    require("./target/scala-2.11/app-backend-fastopt-bundle.js");
+    // require("./target/scala-2.11/app-backend-jsdeps.js")
+    // var app = require("./target/scala-2.11/app-backend-opt-bundle.js").__ScalaJSExportsNamespace.app;
+    console.log(app);
     const facade = app.BackendApp();
     facade.startServer({
         "__dirname": __dirname,
